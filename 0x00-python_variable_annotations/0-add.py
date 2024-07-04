e894e7a55e668d3554a3d1087cc50a2e3517c764
Balamathias <balamathias05@gmail.com>
2024-07-04 11:24:52 +0100

    0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def add(a: float b: float) -> float:
+def add(a: float, b: float) -> float:
     '''
     Function that takes two floats and returns their sum as a float.
     '''
